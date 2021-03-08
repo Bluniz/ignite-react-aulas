c63d52011e704ef7937b6cc1f3885a9458805e14
Lucas Rosa <lucassantosrosa51@gmail.com>
2021-03-08 20:52:09 -0300

    Estrutura ReactJS

--- reactjs/01-github-explorer/src/index.jsx
+++ reactjs/01-github-explorer/src/index.jsx
@@ -1,2 +1,7 @@
-import React from "react";
+import { render } from "react-dom";
 import { App } from "./App";
+
+//! Recebe 2 parametros
+//* 1º Parametro -> O que que eu quero renderizar
+//* 2º Parametro -> Dentro de qual elementoq quero renderizar essa informação
+render(<App />, document.getElementById("root"));
